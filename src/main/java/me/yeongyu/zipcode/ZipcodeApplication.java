bf379090ebf7d323de00dac1ae1db6994d1ba92a
Yeongyu <ygyang0302@gmail.com>
2020-10-23 21:32:13 +0900

    init commit

---
+++ src/main/java/me/yeongyu/zipcode/ZipcodeApplication.java
@@ -0,0 +1,13 @@
+package me.yeongyu.zipcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yeongyu/zipcode/ZipcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yeongyu.zipcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
