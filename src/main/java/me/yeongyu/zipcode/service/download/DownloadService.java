18437e3b63019cfd81fbde86d99a5809a45372d9
Yeongyu <ygyang0302@gmail.com>
2020-10-24 01:58:29 +0900

    config: Korea and Japan zipcode database download url setting service: DownloadFromUrl

---
+++ src/main/java/me/yeongyu/zipcode/service/download/DownloadService.java
@@ -0,0 +1,24 @@
+package me.yeongyu.zipcode.service.download;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+@Service
+public class DownloadService {
+
+    private static void downloadFileFromURL(String urlString, File destination) throws Exception {
+        URL website = new URL(urlString);
+        ReadableByteChannel rbc;
+        rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream(destination);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+        rbc.close();
+    }
+
+}
